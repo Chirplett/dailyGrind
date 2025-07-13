de34d4b5848e27f351fd23d37ac2ba9cea3977aa
Chirplett <chirplett@gmail.com>
2025-07-14 06:18:06 +0900

    Added images, titles, subtitles, and date labels in each cell at VC

--- day011/day011/MagazineTableViewController.swift
+++ day011/day011/MagazineTableViewController.swift
@@ -6,22 +6,23 @@
 //
 
 import UIKit
-import 
+import Kingfisher
 
 
 class MagazineTableViewController: UITableViewController {
     
     var magazineContents = MagazineInfo().magazine
     
+    let dateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationItem.title = "매거진"
+        self.navigationItem.title = "SeSAC TRAVEL"
 
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(#function)
         return magazineContents.count
     }
     
@@ -29,31 +30,37 @@ class MagazineTableViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "eachContentCell", for: indexPath) as! MagazineTableViewCell
         
-        cell.mainThumbnailImageView
-        cell.mainThumbnailImageView
+        let url = URL(string: magazineContents[indexPath.row].photo_image)!
+        
+        
+        cell.mainThumbnailImageView.kf.setImage(with: url)
+        cell.mainThumbnailImageView.contentMode = .scaleAspectFill
+        cell.mainThumbnailImageView.layer.cornerRadius = 10
         
         cell.titleLabel.text = magazineContents[indexPath.row].title
-        cell.titleLabel
+        cell.titleLabel.numberOfLines = 0
+        cell.titleLabel.textAlignment = .left
+        cell.titleLabel.font = .systemFont(ofSize: 22, weight: .black)
         
         cell.subtitleLabel.text = magazineContents[indexPath.row].subtitle
-        cell.subtitleLabel
+        cell.subtitleLabel.numberOfLines = 1
+        cell.subtitleLabel.textAlignment = .left
+        cell.subtitleLabel.font = .systemFont(ofSize: 16, weight: .regular)
+        cell.subtitleLabel.textColor = .gray
         
-        cell.dateLabel
-        cell.dateLabel
+        dateFormatter.dateFormat = "yyMMdd"
+        let currentDate = dateFormatter.date(from: magazineContents[indexPath.row].date)
         
+        dateFormatter.dateFormat = "yy년 MM월 dd일"
+        let updatedDate = dateFormatter.string(from: currentDate!)
+        
+        cell.dateLabel.text = updatedDate
+        cell.dateLabel.font = .systemFont(ofSize: 14, weight: .regular)
+        cell.dateLabel.textColor = .gray
+        
+        return cell
         
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
 
