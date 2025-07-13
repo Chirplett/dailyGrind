97dd265555601f08c1722f8dd78b7367000bdde6
Chirplett <chirplett@gmail.com>
2025-07-14 02:33:39 +0900

    Updated MagazineTableViewCell and MagazineTableVC

---
+++ day011/day011/MagazineTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  MagazineTableViewCell.swift
+//  day011
+//
+//  Created by Jude Song on 7/14/25.
+//
+
+import UIKit
+
+class MagazineTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet var mainThumbnailImageView: UIImageView!
+    
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var subtitleLabel: UILabel!
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+//    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
