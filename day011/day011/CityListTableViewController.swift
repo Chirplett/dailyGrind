c7735a68e9be6e8f76639dcbe496051c440e9a56
Chirplett <chirplett@gmail.com>
2025-07-14 20:51:26 +0900

    CityListTableVC - Added Kingfisher. Set logics for detailed information. Renamed structs. CityRowTableViewCell - Connected outlets for view (labels, button, and imageview) TravelInfo - Renamed some models in struct

---
+++ day011/day011/CityListTableViewController.swift
@@ -0,0 +1,85 @@
+//
+//  CityListTableViewController.swift
+//  day011
+//
+//  Created by Jude Song on 7/14/25.
+//
+
+import UIKit
+import Kingfisher
+
+class CityListTableViewController: UITableViewController {
+    
+    let numberFormatter = NumberFormatter()
+
+    var cityData = TravelInfo().travel
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "도시 상세 정보"
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cityData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CityRowTableViewCell", for: indexPath) as! CityRowTableViewCell
+        
+        let url = URL(string: cityData[indexPath.row].travel_image ?? "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/640px-No-Image-Placeholder.svg.png")
+        
+        cell.titleLabel.text = cityData[indexPath.row].title
+        cell.titleLabel.numberOfLines = 0
+        cell.titleLabel.textAlignment = .left
+        cell.titleLabel.font = .systemFont(ofSize: 18, weight: .heavy)
+        
+        cell.subtitleLabel.text = cityData[indexPath.row].subtitle ?? ""
+        cell.subtitleLabel.numberOfLines = 0
+        cell.subtitleLabel.textAlignment = .left
+        cell.subtitleLabel.font = .systemFont(ofSize: 14, weight: .medium)
+        cell.subtitleLabel.textColor = .gray
+        
+        let cityRating = cityData[indexPath.row].rating ?? 0.0
+        
+        if cityRating == 5.0 {
+            cell.ratingAndSavesLabel.text = "★★★★★"
+        } else if cityRating >= 4.0 && cityRating <= 4.9 {
+            cell.ratingAndSavesLabel.text = "★★★★☆"
+        } else if cityRating >= 3.0 && cityRating <= 3.9 {
+            cell.ratingAndSavesLabel.text = "★★★☆☆"
+        } else if cityRating >= 2.0 && cityRating <= 2.9 {
+            cell.ratingAndSavesLabel.text = "★★☆☆☆"
+        } else if cityRating >= 1.0 && cityRating <= 1.9 {
+            cell.ratingAndSavesLabel.text = "★☆☆☆☆"
+        } else {
+            cell.ratingAndSavesLabel.text = "☆☆☆☆☆"
+        }
+
+        var citySaves = cityData[indexPath.row].saves ?? 0
+        
+        numberFormatter.numberStyle = .decimal
+        let numberedSaves = numberFormatter.string(from: NSNumber(value: citySaves))!
+        
+        cell.ratingAndSavesLabel.textColor = .gray
+        cell.ratingAndSavesLabel.text! += " · 저장 \(numberedSaves)"
+        cell.ratingAndSavesLabel.font = .systemFont(ofSize: 12, weight: .regular)
+        
+        if cityData[indexPath.row].like == true {
+            cell.likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+        } else {
+            cell.likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+        }
+        cell.likeButton.tintColor = .white
+        
+        cell.cityThumbnailImageView.kf.setImage(with: url)
+        cell.cityThumbnailImageView.contentMode = .scaleAspectFill
+        cell.cityThumbnailImageView.layer.cornerRadius = 10
+        
+        return cell
+        
+        
+    }
+
+}
---
+++ day011/day011/CityRowTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  CityRowTableViewCell.swift
+//  day011
+//
+//  Created by Jude Song on 7/14/25.
+//
+
+import UIKit
+
+class CityRowTableViewCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var subtitleLabel: UILabel!
+    
+    @IBOutlet var ratingAndSavesLabel: UILabel!
+    
+    @IBOutlet var likeButton: UIButton!
+    
+    @IBOutlet var cityThumbnailImageView: UIImageView!
+    
+    //    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
