0ebc50aff8a95f1c80f879531fad77187e9f2dcd
Chirplett <chirplett@gmail.com>
2025-07-15 00:45:40 +0900

    Made views for Game369 tab

--- day011/day011/Game369ViewController.swift
+++ day011/day011/Game369ViewController.swift
@@ -8,22 +8,48 @@
 import UIKit
 
 class Game369ViewController: UIViewController {
-
+    
+    @IBOutlet var putNumberTextField: UITextField!
+    @IBOutlet var showNumbersTextView: UITextView!
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        putNumberTextField.text = ""
+        putNumberTextField.placeholder = "최대 숫자를 입력해주세요"
+        putNumberTextField.keyboardType = .default
+        putNumberTextField.borderStyle = .bezel
+        putNumberTextField.layer.borderWidth = 3
+        putNumberTextField.layer.borderColor = UIColor.systemGray5.cgColor
+        putNumberTextField.layer.cornerRadius = 5
+        putNumberTextField.font = UIFont.systemFont(ofSize: 16)
+        
+        showNumbersTextView.text = ""
+        
+        
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func returnButtonTapped(_ sender: UITextField) {
+        
+        if let typedText = putNumberTextField.text, !typedText.isEmpty,
+            let finalNumber = Int(typedText) {
+                
+                var resultText = ""
+                var clapCount = 0
+                
+                for i in 1...finalNumber {
+                    var num = i
+                    var
+                }
+            }
+         
+        }
+        
     }
-    */
+    
+    
+
 
 }
