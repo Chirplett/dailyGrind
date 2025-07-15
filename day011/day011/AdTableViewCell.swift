9c3a84b8fa9c910bc1cb18cfc2f1ee145f6ec72a
Chirplett <chirplett@gmail.com>
2025-07-15 16:18:35 +0900

    Added AdTableViewCell.swift and .xib

---
+++ day011/day011/AdTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AdTableViewCell.swift
+//  day011
+//
+//  Created by Jude Song on 7/15/25.
+//
+
+import UIKit
+
+class AdTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
