85a93a55821982f69cd4bfb857e71c2409048ebb
Chirplett <chirplett@gmail.com>
2025-07-16 22:38:15 +0900

    feat: style CityPopularTableViewCell and define City struct from CityInfo
    - Styled labels, imageView, and views in CityPopularTableViewCell.xib
    - Added City struct model based on CityInfo data

--- day011/day011/CityPopularTableViewCell.swift
+++ day011/day011/CityPopularTableViewCell.swift
@@ -18,8 +18,27 @@ class CityPopularTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        cityNamesLabel.text = ""
+        cityNamesLabel.textColor = .white
+        cityNamesLabel.font = .systemFont(ofSize: 24, weight: .heavy)
+        cityNamesLabel.textAlignment = .center
         
+        cityImageView.contentMode = .scaleAspectFit
+        cityImageView.layer.cornerRadius = 12
+        cityImageView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMaxYCorner]
+        cityImageView.clipsToBounds = true
         
+        cityExplainBgView.backgroundColor = .systemGray6
+        cityExplainBgView.layer.opacity = 0.5
+        cityExplainBgView.layer.cornerRadius = 12
+        cityExplainBgView.layer.maskedCorners = [.layerMaxXMaxYCorner]
+        cityExplainBgView.clipsToBounds = true
+        
+        cityExplainLabel.text = ""
+        cityExplainLabel.textColor = .white
+        cityExplainLabel.font = .systemFont(ofSize: 12, weight: .medium)
+        cityExplainLabel.textAlignment = .left
+
         
     }
 
